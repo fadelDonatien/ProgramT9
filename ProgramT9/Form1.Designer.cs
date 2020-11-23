e76665f61ae7daab927e6fea36de2b59f92123c0
FadelDonatien <fadel.donatien@gmail.com>
2020-11-23 23:17:57 +0100

    ProjectT9
    Program T9 by Fadel Donatien

---
+++ ProgramT9/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace ProgramT9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbInput = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lbOutput = new System.Windows.Forms.Label();
+            this.btOK = new System.Windows.Forms.Button();
+            this.btAnnuler = new System.Windows.Forms.Button();
+            this.lbAffichOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbInput
+            // 
+            this.lbInput.AutoSize = true;
+            this.lbInput.Location = new System.Drawing.Point(27, 59);
+            this.lbInput.Name = "lbInput";
+            this.lbInput.Size = new System.Drawing.Size(44, 13);
+            this.lbInput.TabIndex = 1;
+            this.lbInput.Text = "Entrée :";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(89, 59);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(250, 20);
+            this.tbInput.TabIndex = 2;
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.AutoSize = true;
+            this.lbOutput.Location = new System.Drawing.Point(27, 141);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(46, 13);
+            this.lbOutput.TabIndex = 3;
+            this.lbOutput.Text = "Résultat";
+            // 
+            // btOK
+            // 
+            this.btOK.Location = new System.Drawing.Point(89, 215);
+            this.btOK.Name = "btOK";
+            this.btOK.Size = new System.Drawing.Size(75, 23);
+            this.btOK.TabIndex = 5;
+            this.btOK.Text = "OK";
+            this.btOK.UseVisualStyleBackColor = true;
+            this.btOK.Click += new System.EventHandler(this.btOK_Click);
+            // 
+            // btAnnuler
+            // 
+            this.btAnnuler.Location = new System.Drawing.Point(236, 215);
+            this.btAnnuler.Name = "btAnnuler";
+            this.btAnnuler.Size = new System.Drawing.Size(75, 23);
+            this.btAnnuler.TabIndex = 6;
+            this.btAnnuler.Text = "Annuler";
+            this.btAnnuler.UseVisualStyleBackColor = true;
+            this.btAnnuler.Click += new System.EventHandler(this.btAnnuler_Click);
+            // 
+            // lbAffichOutput
+            // 
+            this.lbAffichOutput.AutoSize = true;
+            this.lbAffichOutput.Location = new System.Drawing.Point(89, 141);
+            this.lbAffichOutput.Name = "lbAffichOutput";
+            this.lbAffichOutput.Size = new System.Drawing.Size(0, 13);
+            this.lbAffichOutput.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 257);
+            this.Controls.Add(this.lbAffichOutput);
+            this.Controls.Add(this.btAnnuler);
+            this.Controls.Add(this.btOK);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.lbInput);
+            this.Name = "Form1";
+            this.Text = "Programme T9 by Fadel Donatien 2020";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbInput;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label lbOutput;
+        private System.Windows.Forms.Button btOK;
+        private System.Windows.Forms.Button btAnnuler;
+        private System.Windows.Forms.Label lbAffichOutput;
+    }
+}
+
---
+++ ProgramT9/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProgramT9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        /// <summary>
+        /// Normalisation des chaines.
+        /// </summary>
+        /// <param name="supText"></param>
+        /// <returns></returns>
+        public string NormalisationChaine(string supText)
+        {
+            string normalText = supText.Normalize(NormalizationForm.FormD);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char normCh in normalText)
+            {
+                UnicodeCategory uc = CharUnicodeInfo.GetUnicodeCategory(normCh);
+
+                if (uc != UnicodeCategory.NonSpacingMark)
+                {
+                    sb.Append(normCh);
+                }
+            }
+
+            return sb.ToString().Normalize(NormalizationForm.FormC);
+        }
+
+        /// <summary>
+        /// Encodage de la chaine saisi en entrée
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        public string StringEncode(string inputString)
+        {
+            // Normalizer et mettre en minuscule les chaines en entrer.
+            string resultEncode = this.NormalisationChaine(inputString).ToLower();
+            // Supprimer les chiffres
+            resultEncode = Regex.Replace(resultEncode, "[0-9]", string.Empty);
+
+            //Chiffre 0
+            resultEncode = Regex.Replace(resultEncode, @"\s", "0"); // On remplace les espaces par le chiffre 0
+            //Chiffre 1
+            resultEncode = Regex.Replace(resultEncode, "[,]", "1"); //On appuye 1 fois la touche 1 pour avoir la virgule
+            resultEncode = Regex.Replace(resultEncode, "[?]", "11"); //On appuye 2 fois la touche 1 pour avoir le point d'intérogation
+            resultEncode = Regex.Replace(resultEncode, "[!]", "111"); //On appuye 3 fois la touche 1 pour avoir le point d'exclamation
+            //Chiffre 2
+            resultEncode = Regex.Replace(resultEncode, "[a]", "2"); //On appuye 1 fois la touche 2 pour avoir la lettre 'a'
+            resultEncode = Regex.Replace(resultEncode, "[b]", "22");//On appuye 2 fois la touche 2 pour avoir la lettre 'b'
+            resultEncode = Regex.Replace(resultEncode, "[c]", "222");//On appuye 3 fois la touche 2 pour avoir la lettre 'c'
+            //Chiffre 3
+            resultEncode = Regex.Replace(resultEncode, "[d]", "3");
+            resultEncode = Regex.Replace(resultEncode, "[e]", "33");
+            resultEncode = Regex.Replace(resultEncode, "[f]", "333");
+            //Chiffre 4
+            resultEncode = Regex.Replace(resultEncode, "[g]", "4");
+            resultEncode = Regex.Replace(resultEncode, "[h]", "44");
+            resultEncode = Regex.Replace(resultEncode, "[i]", "444");
+            //Chiffre 5
+            resultEncode = Regex.Replace(resultEncode, "[j]", "5");
+            resultEncode = Regex.Replace(resultEncode, "[k]", "55");
+            resultEncode = Regex.Replace(resultEncode, "[l]", "555");
+            //Chiffre 6
+            resultEncode = Regex.Replace(resultEncode, "[m]", "6");
+            resultEncode = Regex.Replace(resultEncode, "[n]", "66");
+            resultEncode = Regex.Replace(resultEncode, "[o]", "666");
+            //Chiffre 7
+            resultEncode = Regex.Replace(resultEncode, "[p]", "7");//On appuye 1 fois la touche 7 pour avoir la lettre 'p'
+            resultEncode = Regex.Replace(resultEncode, "[q]", "77"); //On appuye 2 fois la touche 7 pour avoir la lettre 'q'
+            resultEncode = Regex.Replace(resultEncode, "[r]", "777");//On appuye 3 fois la touche 7 pour avoir la lettre 'r'
+            resultEncode = Regex.Replace(resultEncode, "[s]", "7777");//On appuye 4 fois la touche 7 pour avoir la lettre 's'
+            //Chiffre 8
+            resultEncode = Regex.Replace(resultEncode, "[t]", "8");
+            resultEncode = Regex.Replace(resultEncode, "[u]", "88");
+            resultEncode = Regex.Replace(resultEncode, "[v]", "888");
+            //Chiffre 9
+            resultEncode = Regex.Replace(resultEncode, "[w]", "9");
+            resultEncode = Regex.Replace(resultEncode, "[x]", "99");
+            resultEncode = Regex.Replace(resultEncode, "[y]", "999");
+            resultEncode = Regex.Replace(resultEncode, "[z]", "9999");
+
+            // Replacer par un espace les autres caractères spciaux
+            resultEncode = Regex.Replace(resultEncode, "[^0-9]", " ");
+
+            return resultEncode;
+        }
+
+        private void btOK_Click(object sender, EventArgs e)
+        {
+            string strInput = tbInput.Text.ToString();
+            if (!string.IsNullOrEmpty(strInput))
+            {
+                lbAffichOutput.Text = StringEncode(strInput);
+            }
+            else
+                MessageBox.Show("Le champs en etrée est vide", "ERREUR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void btAnnuler_Click(object sender, EventArgs e)
+        {
+            tbInput.Text = string.Empty;
+            lbAffichOutput.Text = string.Empty;
+        }
+    }
+}
